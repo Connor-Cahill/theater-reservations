73525563c6ac91c913d391b9171f73a5bddf0fde
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-11-12 14:42:25 -0800

    model Theater update

--- models/Theater.js
+++ models/Theater.js
@@ -1,17 +1,24 @@
 const Session = require('./Session.js')
 
 
-class Theater{
+class Theater {
 
-    constructor(name){
-
+    constructor(name) {
         this.name = name;
         this.sessions = [];
-        
+    }
+
+    /**
+     * This method allows us to append a session to our theater class
+     * @param {*} movieID - Unique ID 
+     * @param {*} name - Name of Movie
+     */
+    
+    addSession(movieID, name) {
+        this.sessions.push({ movieID: movieID, name: name });
     }
 
 
 }
-
 
 module.exports = Theater;
